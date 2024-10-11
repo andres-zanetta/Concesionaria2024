ce82228ed2dba0c6e067ee8a518d891b956592a4
facupereyrat <faucpereyrat@outlook.com>
2024-10-10 21:15:14 -0300

    Agrego al program los servicios de repositorios (tipoplan,vehiculo,adjudicacion)

--- Concesionaria2024.Shared/DTO/FacundoDTO/CrearAdjudicacionDTO.cs
+++ Concesionaria2024.Shared/DTO/FacundoDTO/CrearAdjudicacionDTO.cs
@@ -1,11 +1,15 @@
-﻿namespace Concesionaria2024.Shared.DTO.FacundoDTO;
-
-public class CrearAdjudicacionDTO
+﻿namespace Concesionaria2024.Shared.DTO
 {
-    public string? Detalle { get; set; }
-    public DateTime FechaAdjudicacion { get; set; }
-    public bool AutoEntregado { get; set; }
-    public string PatenteVehiculo { get; set; }
-    public int VehiculoId { get; set; }
-    public int PlanVendidoId { get; set; }
+    public class CrearAdjudicacionDTO
+    {
+        public string? Detalle { get; set; }
+        public DateTime FechaAdjudicacion { get; set; }
+        public bool AutoEntregado { get; set; }
+        public string PatenteVehiculo { get; set; }
+        public int VehiculoId { get; set; }
+        public int PlanVendidoId { get; set; }
+        public int ClienteId { get; set; } 
+        public decimal Precio { get; set; } 
+        public string? Vendedor { get; set; } 
+    }
 }
