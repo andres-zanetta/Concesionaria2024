5e3557fc950f5f10362f75fd39435a4b6ace711b
Gino Farias <fariasgino@yahoo.com>
2024-10-19 19:48:31 -0300

    Feat(Client): Se incorpora el proyecto Client.

--- Concesionaria.Server/Program.cs
+++ Concesionaria.Server/Program.cs
@@ -5,12 +5,20 @@ using Concesionaria.Server.Repositorio.AndresRepositorios;
 using Concesionaria.Server.Repositorio.FacundoRepositorios;
 using Concesionaria.Server.Repositorio.GinoRepositorios;
 using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(x=>x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+
+// Servicio para el Client
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -48,9 +56,13 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseBlazorFrameworkFiles();
+app.UseRouting();
+app.UseStaticFiles();
+app.MapRazorPages();
 app.UseAuthorization();
 
 app.MapControllers();
+app.MapFallbackToFile("index.html");
 
 app.Run();
