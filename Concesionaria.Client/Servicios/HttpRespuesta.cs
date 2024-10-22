14c591e7ee829a79ed278a41ee3e33f1cfadeea2
Gino Farias <fariasgino@yahoo.com>
2024-10-22 10:49:16 -0300

    Feat(Servicios): Se agrega servicio HTTP y respuesta HTTP en el Client.

---
+++ Concesionaria.Client/Servicios/HttpRespuesta.cs
@@ -0,0 +1,17 @@
+﻿namespace Concesionaria.Client.Servicios
+{
+    public class HttpRespuesta<O>
+    {
+        public HttpRespuesta(O respuesta, bool error, HttpResponseMessage httpResponseMessage)
+        {
+            Respuesta = respuesta;
+            Error = error;
+            this.httpResponseMessage = httpResponseMessage;
+        }
+
+        public O Respuesta { get; }
+        public bool Error { get; }
+        public HttpResponseMessage httpResponseMessage { get; set; }
+
+    }
+}
---
+++ Concesionaria.Client/Servicios/HttpServicio.cs
@@ -0,0 +1,35 @@
+﻿
+using System.Text.Json;
+
+namespace Concesionaria.Client.Servicios
+{
+    public class HttpServicio
+    {
+        private readonly HttpClient http;
+
+        public HttpServicio(HttpClient http)
+        {
+            this.http = http;
+        }
+
+        public async Task<HttpRespuesta<O>> Get<O>(string url)
+        {
+            var response = await http.GetAsync(url);
+            if(response.IsSuccessStatusCode)
+            {
+                var respuesta = await DesSerealizar<O>(response);
+                return new HttpRespuesta<O>(respuesta, false, response);
+            }
+            else
+            {
+                return new HttpRespuesta<O>(default, true, response);
+            }
+        }
+
+        private async Task<O> DesSerealizar<O>(HttpResponseMessage response)
+        {
+            var respuesta = await response.Content.ReadAsStringAsync();
+            return JsonSerializer.Deserialize<O>(respuesta, new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
+        }
+    }
+}
