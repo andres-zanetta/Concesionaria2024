66db890fc9cd00141da3cd8b95acb18ed828fe0d
Bruno Sponton <brunogsponton@hotmail.com>
2024-11-10 15:06:32 -0300

    feat(IHttpServicio): se añade un Post feat(BrunoPages): se añade page CrearPlanVendido

--- Concesionaria.Client/Servicios/IHttpServicio.cs
+++ Concesionaria.Client/Servicios/IHttpServicio.cs
@@ -4,5 +4,7 @@ namespace Concesionaria.Client.Servicios
     public interface IHttpServicio
     {
         Task<HttpRespuesta<O>> Get<O>(string url);
+
+        Task<HttpRespuesta<object>> Post<O>(string url, O entidad);
     }
 }
